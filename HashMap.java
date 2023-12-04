f28c6647dde258b17616be73cfe9fddaaf3739b4
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-12-04 13:36:07 +0530

    Added: HashTable UC2

--- HashMap.java
+++ HashMap.java
@@ -1,19 +1,23 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class HashMap {
     public static void main(String[] args) {
-        String str = "To be or not to be";
+        String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
         String[] arr = str.toLowerCase().split(" ");
-        MyHashMap<String,Integer> mp = new MyHashMap<>();
+        HashMapImplement<String,Integer> mp = new HashMapImplement<>();
         for(String s:arr){
-            int freq = mp.get(s);
-            if(freq == 0){
-                mp.put(s,freq+1);
+            MyMapNode<String, Integer> freq = mp.get(s);
+            if(freq == null){
+                mp.put(s,1);
             }
             else {
-                mp.update(s,freq+1);
+                mp.put(s,freq.val+1);
             }
         }
+        System.out.print("Frequency of word 'paranoid' : ");
+        System.out.println(mp.get("paranoid").val);
+        System.out.println("Freq of each word: ");
         mp.print();
     }
 }
@@ -28,27 +32,62 @@ class MyMapNode<K, V>{
     }
 }
 
-class MyHashMap<K,V>{
+class HashMapImplement<K,V>{
+    ArrayList<MyLinkedMap<K,V>> arr;
+    int numBuckets;
+    HashMapImplement(){
+        this.numBuckets = 10;
+        arr = new ArrayList<>(this.numBuckets);
+        for (int i=0;i<this.numBuckets;i++){
+            arr.add(null);
+        }
+    }
+    public int hashFunction(K key){
+        return Math.abs(key.hashCode() % this.numBuckets);
+    }
+    public MyMapNode<K,V> get(K key){
+        int hash = hashFunction(key);
+        MyLinkedMap<K,V> mp= arr.get(hash);
+        if(mp == null){
+            return null;
+        }
+        return mp.search(key);
+    }
+    public void put(K key,V val){
+        int hash = hashFunction(key);
+        MyLinkedMap<K,V> mp= arr.get(hash);
+        if (mp == null){
+            mp = new MyLinkedMap<>();
+        }
+        if (get(key) == null){
+            mp.put(key,val);
+        }
+        else {
+            mp.update(key,val);
+        }
+        arr.set(hash,mp);
+    }
+    public void print(){
+        for(MyLinkedMap<K,V> node: arr){
+            if (node == null){
+                continue;
+            }
+            node.print();
+        }
+    }
+}
+
+class MyLinkedMap<K,V>{
     MyMapNode<K,V> head, tail;
-    MyHashMap(){
+    MyLinkedMap(){
         head = null;
         tail = null;
     }
-    public V get(K key){
-        MyMapNode<K,V> node = search(key);
-        if(node == null){
-            return (V) Integer.valueOf(0);
-        }
-        return node.val;
+    public MyMapNode<K, V> get(K key){
+        return search(key);
     }
     public MyMapNode<K,V> search(K key){
         MyMapNode<K,V> ptr = head;
-        if (ptr == null){
-            return null;
-        }
-        if(ptr.key == key){
-            return ptr;
-        }
         while (ptr != null && !ptr.key.equals(key)){
             ptr = ptr.next;
         }
@@ -60,7 +99,7 @@ class MyHashMap<K,V>{
     }
     public void put(K key, V val){
         MyMapNode<K,V> newNode = new MyMapNode<>(key,val);
-        if (tail == null){
+        if (head==null && tail == null){
             head = newNode;
             tail = head;
         }
@@ -71,9 +110,14 @@ class MyHashMap<K,V>{
     }
     public void print(){
         MyMapNode<K,V> ptr = head;
+        if (ptr == null){
+            System.out.println("Empty map");
+            return;
+        }
         while (ptr != null){
-            System.out.println(ptr.key+" : "+ptr.val);
+            System.out.print(ptr.key+" : "+ptr.val+" , ");
             ptr = ptr.next;
         }
+        System.out.println();
     }
 }
